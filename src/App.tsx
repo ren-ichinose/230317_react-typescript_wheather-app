2123b48ce48158a14efd71fb455f55b7be3ae028
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-17 23:43:50 +0900

    App.tsxの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Results from "./components/Results";
+import Title from "./components/Title";
+
+interface ResultState {
+    country: string;
+    cityName: string;
+    temperature: string;
+    conditionText: string;
+    icon: string;
+}
 
 function App() {
-  return (
+  const [ city, setCity ] = useState<string>("");
+  const [ result, setResult ] = useState<ResultState>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  })
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=London&aqi=no`)
+      .then(res => res.json())
+      .then(data => { 
+        setResult({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.icon,
+        })
+    })
+  }
+  return (  
     <div className="App">
+      <Title />
+      <Form 
+        setCity = { setCity }
+        getWeather = { getWeather }/>
+      <Results result = { result } />
     </div>
   );
 }
