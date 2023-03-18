102e969d6697c0a6d587cc25a5d3daee5fd89c13
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-18 15:38:42 +0900

    Results.tsxの修正（データ表示）

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -13,6 +13,11 @@ const Results = ({ result }: ResultProps) => {
   return (
     <div>
       <h2>気象データ</h2>
+      { country && <div>{ country }</div> }
+      { cityName && <div>{ cityName }</div> }
+      { temperature && <div>{ temperature }<span>℃</span></div> }
+      { conditionText && <div><img src={icon} alt="icon"/> <span>{ conditionText }</span></div> }
+
     </div>
   );
 };
